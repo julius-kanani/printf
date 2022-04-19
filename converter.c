0b5d11c685cbacc4923578a18dac13dfa9b50732
Bram-Wel <welbram@gmail.com>
2022-04-19 22:02:03 +0000

    .

--- converter.c
+++ converter.c
@@ -4,8 +4,9 @@
  * convert - converts number and base into string
  * @num: input number
  * @base: input base
- * @lowercase: flag if hexa values need to be lowercase
- * Return: result string
+ * @lowercase: flag if hexa values need to be lowercase.
+ *
+ * Return: Pointer to result string.
  */
 char *convert(unsigned long int num, int base, int lowercase)
 {
--- print_address.c
+++ print_address.c
@@ -4,8 +4,9 @@
  * print_address - prints address of input in hexa format
  * @l: va_list arguments from _printf
  * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
- * Return: number of char printed
+ * if a flag is passed to _printf.
+ *
+ * Return: Number of characters printed.
  */
 int print_address(va_list l, flags_t *f)
 {

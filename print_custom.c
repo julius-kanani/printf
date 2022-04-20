e9d29af0dad9d33660340e69ffa54fe04a2d6d37
Bram-Wel <welbram@gmail.com>
2022-04-20 00:13:05 +0000

    Update documentation.

--- print_custom.c
+++ print_custom.c
@@ -42,7 +42,8 @@ int print_bigS(va_list l, flags_t *f)
  * print_rev - prints a string in reverse
  * @l: argument from _printf
  * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
+ * if a flag is passed to _printf.
+ *
  * Return: length of the printed string
  */
 int print_rev(va_list l, flags_t *f)
@@ -67,7 +68,8 @@ int print_rev(va_list l, flags_t *f)
  * print_rot13 - prints a string using rot13
  * @l: list of arguments from _printf
  * @f: pointer to the struct flags that determines
- * if a flag is passed to _printf
+ * if a flag is passed to _printf.
+ *
  * Return: length of the printed string
  */
 int print_rot13(va_list l, flags_t *f)
@@ -98,7 +100,8 @@ int print_rot13(va_list l, flags_t *f)
 /**
  * print_percent - prints a percent
  * @l: va_list arguments from _printf
- * @f: pointer to the struct flags in which we turn the flags on
+ * @f: pointer to the struct flags in which we turn the flags on.
+ *
  * Return: number of char printed
  */
 int print_percent(va_list l, flags_t *f)

d3cd141869266772461ad5e33bd0d964059ae6a0
Bram-Wel <welbram@gmail.com>
2022-04-20 01:31:06 +0000

    Revert "."
    This reverts commit 69a6d29453e8a865e13769d39302d239c5658664.

--- get_print.c
+++ get_print.c
@@ -24,7 +24,7 @@ int (*get_print(char s))(va_list, flags_t *)
 		{'b', print_binary},
 		{'o', print_octal},
 		{'R', print_rot13},
-		/*{'r', print_rev},*/
+		{'r', print_rev},
 		{'S', print_bigS},
 		{'p', print_address},
 		{'%', print_percent}
